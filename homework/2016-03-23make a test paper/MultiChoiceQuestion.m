0801617090cacadd0e5290b96c31392c0c6b76fe
李康 <515867764@qq.com>
2016-03-29 22:38:03 +0800

    出一张试卷。统计分数

---
+++ homework/2016-03-23make a test paper/MultiChoiceQuestion.m
@@ -0,0 +1,22 @@
+//
+//  MultiChoiceQuestion.m
+//  考试试卷练习
+//
+//  Created by 李连宇 on 16/3/24.
+//  Copyright © 2016年 itany. All rights reserved.
+//
+
+#import "MultiChoiceQuestion.h"
+
+@implementation MultiChoiceQuestion
+-(void)setArrWithIndex:(int)i andString:(NSString*)str
+{
+    _arr[i] = str;
+}
+-(void)showOption
+{
+    for (int i = 0; i<3; i++) {
+        NSLog(@"%@",_arr[i]);
+    }
+}
+@end
